98213cb03eb00b521264b8e6cea2ea81e4a81a9e
Ahmad Alawad <ahmadalawad@Ahmads-MacBook-Air.local>
2019-05-27 02:07:17 +0300

    Home page is working, created template and route in server

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    """Homepage."""
+    return render_template("mainpage.html")
 
 if __name__ == "__main__":
 	app.run(port=5002, host='0.0.0.0')
